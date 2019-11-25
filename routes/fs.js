b55ad858074aa0f92c6721f0bf7c17b12da7f715
BWH\Chris.Ma <chris.ma@bestwehotel.com>
2019-11-25 20:04:16 +0800

    ‘练习fs

--- routes/fs.js
+++ routes/fs.js
@@ -49,12 +49,69 @@ router.get('/rmdir', function (req,res,next) {
 })
 // fs.writeFile 写入文件
 router.get('/writeFile', function (req,res,next) {
-	fs.writeFile('fs/index.js', '写入内容', err => { // 如果要写入的文件(index.js)不存在, 系统会自动创建一个index.js文件，并将内容写入文件，注意: 该函数不能连续写入，只能写入一次
+	fs.writeFile('fs/index.js', '写入内容', err => { // 如果要写入的文件(index.js)不存在, 系统会自动创建一个index.js文件，并将内容写入文件，注意: 该函数不能连续写入，只能写入一次，不能连续写入
 		if (err) {
 			console.log(err)
 			res.send(err)
 		} else {
 			res.send('写入成功')
+		}
+	})
+})
+// fs.writeFile 连续写入文件
+router.get('/appendFile', function (req,res,next) {
+	fs.appendFile('fs/fs.js', `\n追加内容写入的内容`, err => { // 如果要写入的文件(index.js)不存在, 系统会自动创建一个index.js文件，并将内容连续写入文件
+		if (err) {
+			console.log(err)
+			res.send(err)
+		} else {
+			res.send('追加成功')
+		}
+	})
+})
+// fs.readFile 读取文件
+router.get('/readFile', function (req,res,next) {
+	fs.readFile('fs/fs.js', (err, data) => {
+		if (err) {
+			console.log(err)
+			res.send(err)
+		} else {
+			console.log(data.toString(), 'data')
+			res.send(data.toString())
+		}
+	})
+})
+// fs.readdir 读取目录
+router.get('/readdir', function (req,res,next) {
+	fs.readdir('public', (err, data) => {
+		if (err) {
+			console.log(err)
+			res.send(err)
+		} else {
+			console.log(data,'data')
+			res.send(data.toString())
+		}
+	})
+})
+// fs.readdir 重命名
+router.get('/rename', function (req,res,next) {
+	fs.rename('fs/yuan.js', 'yuanTest.js', (err) => {
+		if (err) {
+			console.log(err)
+			res.send(err)
+		} else {
+			res.send('重命名成功')
+		}
+	})
+})
+// fs.readdir 剪切
+router.get('/renameCut', function (req,res,next) {
+	fs.rename('yuan.js', 'fs/yuan.js', (err) => {
+		if (err) {
+			console.log(err)
+			res.send(err)
+		} else {
+			res.send('剪切成功')
 		}
 	})
 })
