8eb39a22745e8fcfc09eec798b3a2b0872175c45
BWH\Chris.Ma <chris.ma@bestwehotel.com>
2020-01-07 16:05:09 +0800

    引入open中间件，可以直接打开指定页面； package-json 增加"nodemon-start": "nodemon bin/www"命令

--- routes/redis.js
+++ routes/redis.js
@@ -1,99 +1,103 @@
 var express = require('express');
 var router = express.Router();
-
+const open = require('open');
 
 router.post('/set', function(req, res, next) {
 	console.log(req.body, 'req')
-	let key = 'game:task'
-	let value={
-		userid:"12345677",
-		taskid:"1",
-		diamond:"100"
-	}
+	// let key = 'game:task'
+	// let value={
+	// 	userid:"12345677",
+	// 	taskid:"1",
+	// 	diamond:"100"
+	// }
 
-	redisClient.set('1234','10', (err, res1) => {
-		console.log(err, res1, 'hmset');
-		redisClient.expire('1234',60);//60秒自动过期
-		res.json({
-			code: 1,
-			data: {
-				msg: res1
-			}
-		})
+	// open('www.baidu.com'); // 使用默认浏览器打开网页
+	redisClient.set('authmanager:yuan','yuan',(err, data) => {
+		console.log(err, data, 'err, data')
 	})
-
-	redisClient.hmset(key,value,function(err,res){ // 使用hash建数据
-		if(err){
-			console.log(err);
-		}else{
-			redisClient.expire(key,60);//60秒自动过期
-			console.log(res, 'hmset');
-		}
-	});
-
-	redisClient.hmget(key,['userid','taskid'],function(err,res){
-		if(err){
-			console.log(err);
-		}else{
-			console.log(res, 'hmget');
-		}
-	});
-
-
-	redisClient.hset(key,'taskid','2',function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-
-	redisClient.hget(key,'taskid',function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-	redisClient.hgetall(key,function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-
-	redisClient.hexists(key,'taskid',function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-	redisClient.hincrby(key,'diamond',100,function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-
-
-	redisClient.hdel(key,'diamond',function(err,res){
-		if(err){
-			console.log(err);
-		} else{
-			console.log(res, 'hmget')
-		}
-	});
-
-	redisClient.hmget(key,['userid','taskid'],function(err,res){
-		if(err){
-			console.log(err);
-		}else{
-			console.log(res, 'hmget');
-		}
-	});
+	// redisClient.set('1234','10', (err, res1) => {
+	// 	console.log(err, res1, 'hmset');
+	// 	redisClient.expire('1234',60);//60秒自动过期
+	// 	res.json({
+	// 		code: 1,
+	// 		data: {
+	// 			msg: res1
+	// 		}
+	// 	})
+	// })
+	//
+	// redisClient.hmset(key,value,function(err,res){ // 使用hash建数据
+	// 	if(err){
+	// 		console.log(err);
+	// 	}else{
+	// 		redisClient.expire(key,60);//60秒自动过期
+	// 		console.log(res, 'hmset');
+	// 	}
+	// });
+	//
+	// redisClient.hmget(key,['userid','taskid'],function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	}else{
+	// 		console.log(res, 'hmget');
+	// 	}
+	// });
+	//
+	//
+	// redisClient.hset(key,'taskid','2',function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	//
+	// redisClient.hget(key,'taskid',function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	// redisClient.hgetall(key,function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	//
+	// redisClient.hexists(key,'taskid',function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	// redisClient.hincrby(key,'diamond',100,function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	//
+	//
+	// redisClient.hdel(key,'diamond',function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	} else{
+	// 		console.log(res, 'hmget')
+	// 	}
+	// });
+	//
+	// redisClient.hmget(key,['userid','taskid'],function(err,res){
+	// 	if(err){
+	// 		console.log(err);
+	// 	}else{
+	// 		console.log(res, 'hmget');
+	// 	}
+	// });
 });
 
 module.exports = router;
